e905874bbb438f224e6dd14bd21062d16c6537b2
William Chong <me@williamchong.cloud>
2023-06-26 19:49:11 +0800

    ♻️ Use walletconnect v2 for cosmostation mobile

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ import {
 } from './utils/cosmostation';
 import {
   checkIsInCosmostationMobileInAppBrowser,
-  getCosmostationMobileWCConnector,
+  onCosmostationMobileDisconnect,
   initCosmostationMobile,
 } from './utils/cosmostation-mobile';
 import {
@@ -249,9 +249,7 @@ export class LikeCoinWalletConnector {
           break;
 
         case LikeCoinWalletConnectorMethodType.CosmostationMobile:
-          wcConnector = getCosmostationMobileWCConnector({
-            bridge: this.options.cosmostationAppWCBridge,
-          });
+          await onCosmostationMobileDisconnect();
           break;
 
         case LikeCoinWalletConnectorMethodType.LikerId:
